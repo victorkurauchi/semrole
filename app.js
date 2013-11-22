8952b8c658de8abea2bc208c2411ef392d6b6285
Victor Kurauchi <victorkurauchi@gmail.com>
2013-11-22 20:38:43 -0200

    resolvendo conflitos de merge

--- app.js
+++ app.js
@@ -6,20 +6,11 @@
 var express = require('express'),
   routes = require('./routes'),
   api = require('./routes/api'),
-<<<<<<< HEAD
-=======
   beer = require('./routes/beer'),
->>>>>>> a75b6b485088eba638565476ef4165d0dd307fbd
   http = require('http'),
   path = require('path');
 
 var app = module.exports = express();
-<<<<<<< HEAD
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
-=======
-
->>>>>>> a75b6b485088eba638565476ef4165d0dd307fbd
 
 /**
  * Configuration
@@ -53,21 +44,10 @@ if (app.get('env') === 'production') {
 // serve index and view partials
 app.get('/', routes.index);
 app.get('/partials/:name', routes.partials);
-<<<<<<< HEAD
-=======
 app.get('/expose/:dir/:name', routes.expose);
->>>>>>> a75b6b485088eba638565476ef4165d0dd307fbd
 
 // JSON API
 app.get('/api/name', api.name);
-
-<<<<<<< HEAD
-// redirect all others to the index (HTML5 history)
-app.get('*', routes.index);
-
-// Socket.io Communication
-io.sockets.on('connection', require('./routes/socket'));
-=======
 
 app.get('/api/beers', beer.list);
 app.post('/api/beers', beer.create);
@@ -78,17 +58,10 @@ app.delete('/api/beers/:id', beer.delete);
 // redirect all others to the index (HTML5 history)
 app.get('*', routes.index);
 
-
->>>>>>> a75b6b485088eba638565476ef4165d0dd307fbd
-
 /**
  * Start Server
  */
 
-<<<<<<< HEAD
-server.listen(app.get('port'), function () {
-=======
 http.createServer(app).listen(app.get('port'), function () {
->>>>>>> a75b6b485088eba638565476ef4165d0dd307fbd
   console.log('Express server listening on port ' + app.get('port'));
 });
